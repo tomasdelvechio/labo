7a1848477bb98ae9d63fe2f993cb1d29e16da92e
Tomas Delvechio <tomasdelvechio17@gmail.com>
2022-08-13 00:00:30 -0300

    Notebooks actualizadas

--- src/rpart/z101_PrimerModelo.R
+++ src/rpart/z101_PrimerModelo.R
@@ -7,7 +7,7 @@ require("rpart")
 require("rpart.plot")
 
 #Aqui se debe poner la carpeta de la materia de SU computadora local
-setwd("D:\\gdrive\\UBA2022\\")  #Establezco el Working Directory
+setwd("/home/tomas/workspace/uba/dmeyf")  #Establezco el Working Directory
 
 #cargo el dataset
 dataset  <- fread("./datasets/competencia1_2022.csv")
@@ -51,3 +51,4 @@ dir.create( "./exp/KA2001" )
 fwrite( dapply[ , list(numero_de_cliente, Predicted) ], #solo los campos para Kaggle
         file= "./exp/KA2001/K101_001.csv",
         sep=  "," )
+
