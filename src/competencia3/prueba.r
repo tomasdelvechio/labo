881c122028b2d825338debd974609a00be2371ff
Tomas Delvechio <tomasdelvechio17@gmail.com>
2022-10-11 12:43:18 -0300

    pruebas durante clase

---
+++ src/competencia3/prueba.r
@@ -0,0 +1,13 @@
+rm(list = ls())
+gc(verbose = FALSE)
+
+# Librerías necesarias
+require("data.table")
+
+# Poner la carpeta de la materia de SU computadora local
+setwd("/home/tomas/workspace/uba/dmeyf")
+# Poner sus semillas
+semillas <- c(697157, 585799, 906007, 748301, 372871)
+
+# Cargamos el dataset
+dataset <- fread("./datasets/competencia3_2022.csv.gz")
