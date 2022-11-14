99269626afdbee3837e6f6b64292b2cfdb5fef97
Tomas Delvechio <tomasdelvechio17@gmail.com>
2022-11-14 12:23:23 -0300

    se exploran puntos de corte con menor granularidad

--- src/semillerio/z1292_SM_semillero_grafica_comparacion_ganancias_ppc.r
+++ src/semillerio/z1292_SM_semillero_grafica_comparacion_ganancias_ppc.r
@@ -6,13 +6,13 @@ require("data.table")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "ZZ1292_ganancias_semillerio_ppc_50"
+PARAM$experimento <- "ZZ1292_ganancias_semillerio"
 PARAM$exp_input <- "ZZ9410_semillerio"
 
 #PARAM$corte <- 11000 # cantidad de envios
-PARAM$cortes  <- seq( from=  9000,
-                      to=    12000,
-                      by=        500 )
+PARAM$cortes  <- seq( from=  7000,
+                      to=    14000,
+                      by=        250 )
 # FIN Parametros del script
 
 options(error = function() {
@@ -129,6 +129,10 @@ for (corte in PARAM$cortes)
          fill = c("red", "green", "blue"),
          horiz = FALSE
   )
+
+  message(" Pto Corte : ", corte, " -  Dif entre semilla e avg ind: ", max(tb_ganancias[, get(nom_corte_sem)]) - mean_gan_ind )
+  message(" Pto Corte : ", corte, " -  Dif entre min semilla e avg ind: ", min(tb_ganancias[, get(nom_corte_sem)]) - mean_gan_ind )
+
 }
 
 
